e0cbdfe8a192657774dc6dfbb4e658e69b4189b7
Lurain <pcaso5@naver.com>
2018-03-15 23:57:22 +0900

    2018-03-15 #7 Js is hard & useful.

---
+++ main-manu.js
@@ -0,0 +1,22 @@
+document.write('<div class="header">'+
+  '<div class="menu">'+
+    '<div>'+
+      '<div class="menuImg"></div>'+
+      '<div class="menuImg"></div>'+
+      '<div class="menuImg"></div>'+
+    '</div>'+
+    '<ul id="mainMenu">'+
+      '<li>Portpolio'+
+            '<ul>'+
+              '<li>Photoshop</li>'+
+              '<li>Ilustrator</li>'+
+              '<li>Indesign</li>'+
+              '<li>Photograph</li>'+
+              '<li>Video</li>'+
+            '</ul>'+
+      '</li>'+
+      '<li>Blog</li>'+
+      '<li>Youtube</li>'+
+    '</ul>'+
+  '</div>'+
+'</div>');
